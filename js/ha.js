bab533670392a3d00a153269c141ba31959e227a
Ravi Kumar <ravikumar10593@gmail.com>
2020-04-17 15:54:28 +0530

    index.htm & style.css

---
+++ js/ha.js
@@ -0,0 +1,15 @@
+var deg =6;
+	var hr = document.querySelector("#hr");
+  var mn = document.querySelector("#mn");
+  var sc = document.querySelector("#sc");
+
+   setInterval(() => {
+   var day = new Date();
+   var hh = day.getHours() * 30;
+   var mm = day.getMinutes() * deg;
+   var ss = day.getSeconds() * deg;
+
+   hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+   mn.style.transform = `rotateZ(${mm}deg)`;
+   sc.style.transform = `rotateZ(${ss}deg)`;
+})
